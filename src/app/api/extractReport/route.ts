6ea5432cbd2eb4fb02cbf5468ad47e8b9f57ca33
themillenniumfalcon <nishankpr2002@gmail.com>
2025-01-29 12:42:48 +0530

    extractReport API route now working

--- src/app/api/extractReport/route.ts
+++ src/app/api/extractReport/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest, NextResponse } from "next/server"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
@@ -9,14 +10,17 @@ const prompt = `Attached is an image of a clinical report.
 Go over the the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date etc. Make sure to include numerical values and key details from the report, including report title.
 ## Summary: `
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
     const { base64 } = await req.json()
     const filePart = fileToGenerativePart(base64)
 
     const generatedContent = await model.generateContent([prompt, filePart])
 
     const textResponse = generatedContent.response.candidates![0].content.parts[0].text
-    return new Response(textResponse, { status: 200 })
+    return NextResponse.json(
+        { text: textResponse },
+        { status: 200 }
+    )
 }
 
 function fileToGenerativePart(imageData: string) {
